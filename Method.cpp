896d7febe65c219c16cf3fa7437185b46fa892ec
Golikovproger <maksimgolikov19@gmail.com>
2021-12-06 22:07:56 +0300

    res1

---
+++ Method.cpp
@@ -0,0 +1,228 @@
+#include "Method.h"
+#include <string>
+#include <vector>
+
+
+// *********************
+
+const std::vector <funs::funptr_t> methods({ funs::LU, funs::JACOBI, funs::SEIDEL });
+const std::vector <std::string> names({ "LU", "JACOBI", "SEIDEL" });
+
+Matrix funs::LU(const Matrix& A, const Matrix& b, const Options& options)
+{
+    unsigned int size = A.get_cSize();
+    Matrix LU (size, size);
+    Matrix L(size, size);
+    Matrix U(size, size);
+    double temp;
+
+    // Zeroing of Matrix elements U and L. Setting the value 1 to the main diagonal of the Matrix L
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            U(i,j) = 0;
+            L(i, j) = 0;
+        }
+        L(i, i) = 1;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            temp = 0;
+
+            if (i <= j)
+            {
+                // Determination of the elements of the Matrix U
+                for (size_t k = 0; k < i; k++)
+                    temp += L(i, k) * U(k, j);
+                U(i, j) =  A(i, j) - temp;
+            }
+            else
+            {
+                // Determination of the elements of the Matrix L
+                for (size_t k = 0; k < i; k++)
+                    temp += L(i, k) * U(k, j);
+                L(i, j) = (A(i, j) - temp) / U(j, j);
+            }
+
+        }
+    }
+
+    // Writing the LU Matrix in compact form
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            if (i < j)
+              LU(i, j) =  U(i, j);
+            else if (i > j)
+                LU(i, j) = L(i, j);
+        }
+        LU(i, i) =  U(i, i);
+    }
+//бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюх
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана//бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    //бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана бебра понюхана
+    
+    if (LU(0, 0) == 0) {
+        // required = the number of the row in which A[required][0] != 0 
+        unsigned int required;
+        for (required = 1; required < LU.get_rSize(); required++)
+        {
+            if (LU(required, 0) != 0) {
+                double temp;
+                for (size_t col = 0; col < LU.get_cSize(); col++)
+                {
+                    temp = LU(required, col);
+                    LU(required, col) =  LU(required + 1, col);
+                   LU(required + 1, col) =  temp;
+                }
+                break;
+            }
+        }
+    }
+
+    size = LU.get_rSize();
+    Matrix y(b.get_rSize(),b.get_cSize());
+    Matrix x(b.get_rSize(),b.get_cSize());
+
+    // reverse move for matrix L
+    y(0, 0) = b(0, 0);
+    for (size_t k = 1; k < L.get_rSize(); k++)
+    {
+        y(k, 0) = b(k, 0);
+        for (size_t p = 0; p < k; p++)
+            y(k, 0) =  y(k, 0) - L(k, p) * y(p, 0);
+    }
+
+    // reverse move for matrix U
+    const unsigned int sizeForX = U.get_rSize() - 1;
+    x(sizeForX, 0) =  y(sizeForX, 0) / U(sizeForX, sizeForX);
+    for (int k = sizeForX - 1; k >= 0; k--)
+    {
+        x(k, 0) =  y(k, 0);
+        for (size_t p = k + 1; p < sizeForX + 1; p++)
+            x(k, 0) =  x(k, 0) - U(k, p) * x(p, 0);
+        x(k, 0) =  x(k, 0) / U(k, k);
+    }
+
+    return x;
+    
+}
+
+Matrix funs::JACOBI(const Matrix& A, const Matrix& b, const Options& options)
+{
+
+    Matrix x(b.get_rSize(), b.get_cSize());
+    Matrix x_prev(b.get_rSize(), b.get_cSize());
+
+    int maxiter = options.get().maxitr;
+    int accst = options.get().accst;
+
+    Matrix revD(A.get_rSize(), A.get_cSize());
+    for (size_t i = 0; i < A.get_rSize(); i++)
+        revD(i, i) = 1 / A(i, i);
+    Matrix g = revD * b;
+
+    ////////////////////////////////////////// убрать
+    int column = A.get_cSize();
+    int row = A.get_rSize();
+    int size = A.get_cSize();
+    Matrix L(row, column);
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+            if (i > j)
+                L(i, j) = A(i, j);
+    }
+    Matrix U(row, column);
+
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+            if (i < j)
+                U(i, j) = A(i, j);
+    }
+    //////////////////////////////////////
+
+    Matrix B = revD * (L + U);
+    double norm;
+    int iter = 0;
+
+    do {
+        x_prev = x;
+        x = g - B * x_prev;
+        norm = get_norm(x - x_prev);
+        std::cout << iter << std::endl;
+        iter++;
+    } while (norm > accst && maxiter >= iter);
+
+    return x;
+}
+
+Matrix funs::SEIDEL(const Matrix& A, const Matrix& b, const Options& options)
+{
+    Matrix x(b.get_rSize(), b.get_cSize());
+    Matrix x_prev(b.get_rSize(), b.get_cSize());
+
+    double norm;
+    int iter = 0;
+    double sum1, sum2;
+    int maxiter = options.get().maxitr;
+    int accst = options.get().accst;
+
+    do {
+        x_prev = x;
+        for (size_t i = 0; i < x.get_rSize(); i++)
+        {
+            sum1 = 0;
+            sum2 = 0;
+            //std::cout << "i: " << i << std::endl;
+            for (int loop1 = i - 1; loop1 > 0; loop1--)
+            {
+                //std::cout << "loop1: " << loop1 << std::endl;
+                sum1 += A(i, loop1) * x(loop1, 0);
+            }
+
+            //std::cout << "check: " << std::endl;
+            for (int loop2 = i + 1; loop2 < x.get_rSize(); loop2++)
+            {
+                //std::cout << "loop2: " << loop2 << std::endl;
+                sum2 += A(i, loop2) * x_prev(loop2, 0);
+            }
+
+            x(i, 0) = (b(i, 0) - sum1 - sum2) / A(i, i);
+        }
+        norm = get_norm(x - x_prev);
+        std::cout << iter << std::endl;
+        iter++;
+        //std::cout << "norm: "  << norm << std::endl;
+    } while (norm > accst && maxiter >= iter);
+
+    return x;
+}
+
+// *********************
+
+Method::Method() : key(mtd::defult), ptr(methods.at(static_cast<unsigned>(mtd::defult))) {};
+
+Method::Method(const mtd m) : key(m), ptr(methods.at(static_cast<unsigned>(m))) {};
+
+const funs::funptr_t Method::get() const
+{
+    return this->ptr;
+}
+void Method::set(const mtd m)
+{
+    this->key = m;
+    this->ptr = methods.at(static_cast<unsigned>(m));
+}
---
+++ Practical_work_6.cpp
@@ -0,0 +1,110 @@
+﻿// Practical_work_6_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <vector>
+#include "Matrix.h"
+#include "Method.h"
+#include "Options.h"
+#include "Task.h"
+#include "SLE.h"
+#include <time.h>
+
+//const Matrix get_L(const Matrix any);
+//const Matrix get_U(const Matrix any);
+//const double get_norm(const Matrix any);
+//const Matrix yakobi(const Matrix A, const Matrix b, const double eps = 10e-60);
+//const Matrix zeidel(const Matrix A, const Matrix b, const double eps = 10e-60);
+
+enum class ERROR
+{
+    begin,
+    EMPTY = begin,
+    MAIN_CRITERION = 1,
+    SIZE_CHECK = 2,
+
+    end,
+};
+
+//ERROR& operator++(ERROR& e)
+//{
+//    e = ERROR(static_cast<int>(e) + 1);
+//    return e;
+//}
+
+mtd& operator++(mtd& e)
+{
+    e = mtd(static_cast<int>(e) + 1);
+    return e;
+}
+
+int main()
+{
+    std::string fullway2data = "E:\\input.txt";
+    //const Matrix& A = read(fullway2data);
+    Matrix A = read(fullway2data);
+
+    double sum_diag = 0; 
+    ERROR flag = ERROR::EMPTY;
+
+    for (size_t i = 0; i < A.get_rSize(); i++)
+        for (size_t j = 0; j < A.get_cSize(); j++)
+            if (i != j)
+                sum_diag += A(i, j);
+
+    for (size_t i = 0; i < A.get_rSize(); i++)
+        if (A(i, i) < sum_diag)
+            flag = ERROR::MAIN_CRITERION;
+
+    if (flag != ERROR::EMPTY)
+    {
+        std::string report;
+        switch (flag) 
+        {
+            case(ERROR::MAIN_CRITERION):
+                report = "ERROR. THE VALUE OF THE DIAGONAL ELEMENT OF THE MATRIX \nMUST BE GREATER THAN OR EQUAL TO \nTHE SUM OF THE NON-DIAGONAL ELEMENTS.";
+                break;
+            case(ERROR::SIZE_CHECK):
+                break;
+        }
+
+        std::cout << report;
+        return 0;
+    }
+
+
+    Matrix x_exist(A.get_cSize(), 1);
+
+    // filling x_exist with values from 1 to the number of columns of matrix A
+    unsigned int temp = 0;
+    for (size_t i = 0; i < x_exist.get_rSize(); i++)
+    {
+        temp = i + 1;
+        x_exist(i, 0) = temp;
+    }
+
+    // setting the values of the vector of the right part
+    //const Matrix& b = A * x_exist;
+    Matrix b = A * x_exist;
+
+
+
+    tsk a = tsk::A;
+    struct 
+    Task task(A, b);
+   
+    /*mtd aboba = mtd::JACOBI;*/
+    for (mtd i = mtd::begin; i != mtd::end; ++i)
+    {
+       
+        SLE sle(task, Method(i));
+        clock_t start = clock();
+        print(sle.solution());
+        clock_t end = clock();
+        double seconds = (double)(end - start) / CLOCKS_PER_SEC;
+        printf("The time: %f seconds\n", seconds);
+    }
+
+    return 0;
+}
+
